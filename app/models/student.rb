518f1c7a9c74abb01aec7a32bef6c137e3f62d06
Ryan Linton <ryanlntn@gmail.com>
2013-07-13 13:08:19 -0700

    Add some students to seeds and reset db

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,4 @@
 class Student < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :house
   belongs_to :house
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 houses = House.create([{ name: 'Gryffindor' }, { name: 'Hufflepuff' }, { name: 'Ravenclaw' }, { name: 'Slytherin' }])
+Student.create([{ name: 'Harry Potter', house: houses[0] },
+                { name: 'Ron Weasley', house: houses[0] },
+                { name: 'Hermione Granger', house: houses[0] }])
+Student.create([{ name: 'Pomona Sprout', house: houses[1] },
+                { name: 'Nymphadora Tonks', house: houses[1] },
+                { name: 'Cedric Diggory', house: houses[1] }])
+Student.create([{ name: 'Luna Lovegood', house: houses[2] },
+                { name: 'Cho Chang', house: houses[2] },
+                { name: 'Gilderoy Lockhart', house: houses[2] }])
+Student.create([{ name: 'Draco Malfoy', house: houses[3] },
+                { name: 'Gregory Goyle', house: houses[3] },
+                { name: 'Vincent Crabbe', house: houses[3] }])
