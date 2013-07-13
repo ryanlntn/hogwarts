d126a3aa1c40e6ade95ee0ca52787bd2eefada5a
Ryan Linton <ryanlntn@gmail.com>
2013-07-13 15:09:00 -0700

    Fix students/index template and modify routes to include useful helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Hogwarts::Application.routes.draw do
   root to: 'welcome#index'
 
-  get '/houses' => 'houses#index'
-  get '/houses/:id' => 'houses#show'
-  get '/students' => 'students#index'
-  get '/students/new' => 'students#new'
-  post '/students/create' => 'students#create'
-  get '/students/:id' => 'students#show'
+  get '/houses', to: 'houses#index', as: 'houses'
+  get '/houses/:id', to: 'houses#show', as: 'house'
+  get '/students', to: 'students#index', as: 'students'
+  get '/students/new', to: 'students#new'
+  post '/students/create', to: 'students#create'
+  get '/students/:id', to: 'students#show', as: 'student'
 end
